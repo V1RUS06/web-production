91a4c36b48857713da3ed226658da2341a023c17
Denis <d.fomin@spotapp.ru>
2023-02-18 12:32:51 +0300

    Настроил react-testing-library

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-  <div className={classNames(cls.Navbar, {}, [className])}>
-    <div className={cls.links}>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={classNames(cls.mainLink)}>Главная</AppLink>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/about">О сайте</AppLink>
-    </div>
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation();
 
-  </div>
-);
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={classNames(cls.mainLink)}>
+          {t('Главная')}
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+          {t('О сайте')}
+        </AppLink>
+      </div>
+
+    </div>
+  );
+};
